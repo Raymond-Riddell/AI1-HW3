2f507e735ff17749d662aea0200eb308a06a8022
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2023-11-16 12:31:11 -0500

    we now have extra credit for the pancakes

--- pancakes.py
+++ pancakes.py
@@ -20,11 +20,15 @@ def main(args):
     # Parse inputs
     n = args.num  # number of pancakes
     stack = list(range(n))
-    if args.seed is not None:  # randomly shuffle the pancakes initially
-        random.shuffle(stack)
 
     # Make the graphical user interface
     gui = guisetup(stack)
+
+    if args.seed is not None:  # randomly shuffle the pancakes initially
+        random.seed(args.seed)
+        random.shuffle(stack)
+        path = search(stack)[0]
+        simulate(stack, path[::-1], gui)
 
     # Use the graphical user interface
     while True:
@@ -37,6 +41,7 @@ def main(args):
                 pdb.set_trace()
             elif key == 'g':  # run greedy best-first search
                 path = gbfs(gui, stack)
+                simulate(stack, path, gui)
             elif key in [str(i) for i in range(1, n + 1)]:  # manually flip some of the pancakes
                 stack = flip(gui, stack, int(key))
 
@@ -86,14 +91,22 @@ def flip(gui, stack, p, update_gui=True):
 
 def flip_stack(stack, p):
     '''Flip p pancakes in an ordered stack.'''
-    queue = []
+    # strategy here is to take p pancakes off of the stack, add them to a queue, 
+    # and add p items from the queue back onto the stack, which will result in the 
+    # p number of pancakes flipped on the stack
 
+    # temp queue
+    queue = []
     stack_copy = stack.copy()
+
+    # popping p items of the stack
     for _ in range(p):
         queue.insert(0, stack_copy.pop(0))
 
+    # popping all items from the stack back onto the queue
     for _ in range(len(queue)):
         stack_copy.insert(0, queue.pop())
+
     return stack_copy
 
 
@@ -102,23 +115,34 @@ def calc_cost(stack):
     '''Compute the cost h(stack) for a given stack of pancakes.
     Here, we define cost as the number of pancakes in the wrong position.'''
     # ***MODIFY CODE HERE*** (2 lines)
-    h = 0
-    lst = [a for a in range(len(stack))]
+    
+    # iterate through both the given stack, and a solved stack, and then count 
+    # the values that do not match
+    h = 0 # heuristic value
+    correct_spots = [a for a in range(len(stack))] #[0,1,2,3,4,5,6, ... len(stack) - 1]
     for i in range(len(stack)):
-        h += 1 if lst[i] != stack[i] else 0
+        h += 1 if correct_spots[i] != stack[i] else 0
     return h
 
 def gbfs(gui, stack):
-    '''Run greedy best-first search on a stack of pancakes and return the solution path.'''
+    '''Wrapper function for the GBFS calculations'''
     print("Running greedy best-first search...")
+
 
     # Get graphics objects from GUI
     objects = gui.items
     status = None
+    # since objects will be in random order, we have to iterate to specifically
+    # find the text object we are looking to update
     for obj in objects:
         if type(obj) == Text and obj.getText() != "Press a # to flip pancakes, 'g' to run GBFS, Escape to quit":
             status = obj
             break
+
+    # check if we even need to solve the game
+    if stack == [a for a in range(len(stack))]:
+        status.setText("The given state is already solved!")
+        return
 
 
     # Update status text on GUI
@@ -126,55 +150,76 @@ def gbfs(gui, stack):
     time.sleep(0.5)
 
     # ***MODIFY CODE HERE*** (20-25 lines)
-    path, cnt  = search(stack, "", calc_cost(stack), 0)
+    path, cnt  = search(stack)
     
 
     print(f'searched {cnt} paths')
     print(f'solution: {path}')
     status.setText("...search is complete")
+    return path
 
 
-def search(state, path, cost, count):
+def search(state):
+    '''Run greedy best-first search on a stack of pancakes and return the solution path.'''
 
     initial_state = state.copy()
-    
-    # in --> [] out -->
-    path = ""
     cnt = 0
     backpointers = dict()
+
+    # unique id to associate with ever stack state that we encounter
     id = 0
+    
+    # add the starting node to the visited look up table and the queue
     visited = {id:state.copy()} # {id : stack} a look up table for all vistited states
-    solved_state = [a for a in range(len(initial_state))]
 
     # queue consists of ids corresponding to state, and a cost associated with the h(state)
-    queue = deque([[id, calc_cost(visited[id])]])  # items are [id(), h(state)]
+    queue = deque([[id, calc_cost(visited[id])]])  # items are [id(state), h(state)]
     id += 1
 
     while True:
         cnt += 1
 
+        # get current node
         node_id, node_cost = queue.pop()
         node = visited[node_id].copy() # node is a stack of pancakes (integers), and represents the current state
-        if node_cost == 0:
 
+        # check if we have solved the game
+        if node_cost == 0:
+            # if so, generate the path we took by traversing through backpointers
             path = ""
+
+            # we are backward chaining! so we need to reverse our steps untill we are at the starting state
             while node != initial_state:
+                
+                # get parent
                 parent_id = backpointers[node_id]
                 parent = visited[parent_id]
-                move = find_move(node, parent)
+                
+                # find the move made between the current node and the parent
+                move = find_move(node, parent) 
+
+                # update path
                 path += str(move)
+
+                # update the current node
                 node_id = parent_id
                 node = parent
 
-            return path[::-1], cnt
+            return path[::-1], cnt # we want to reverse the path because we are backward chaining
 
+
+        # for the given node, find every possible move we can make
         moves = [a for a in range(2, len(node) + 1)]
 
+        # for each move, pruduce a resulting child
         for move in moves:
             child = flip_stack(node.copy(), move)
             child_cost = calc_cost(child)
 
+            # check if we have visited the child
             if child not in visited.values():
+
+                # if we have not, update the visited, and add them to the queue
                 child_id = id
                 id += 1
                 queue.appendleft([child_id, child_cost])
@@ -186,21 +231,13 @@ def search(state, path, cost, count):
         queue = deque(sorted(queue, key=lambda pair : pair[1], reverse=True))
 
 
-def find_children(state):
-
-    children = []
-
-    pass
-def simulate(stack, path):
+def simulate(stack, path, gui):
     '''Simulate the flipping of pancakes to determine the resulting stack.'''
     fakestack = stack.copy()  # make a copy so we don't actually change the real stack
     for action in path:
-        try:
-            p = int(action)  # how many pancakes are we trying to flip?
-            for i in range(1, p // 2 + 1):
-                fakestack[-i], fakestack[- (p - i + 1)] = fakestack[-(p - i + 1)], fakestack[-i]
-        except:
-            print("INVALID ACTION: Check code")
+        stack = flip_stack(stack, int(action))
+        draw_pancakes(gui, stack, len(stack))
+        time.sleep(0.1)
 
     return fakestack
 
@@ -258,6 +295,9 @@ def draw_pancakes(gui, stack, n):
 
 def find_move(state1, state2):
     '''Looks at two board states, and determines how many pancakes need to be flipped to produce the other state'''
+
+    # here, we iterate through both states backward, and stop counting when we find 
+    # values that do not match
     count_same = 0
     i = len(state1) - 1
 
